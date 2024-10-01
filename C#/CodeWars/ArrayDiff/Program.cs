c0c238f15aeb89dfe761588b126fc110d9a1a5e4
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-01 14:36:15 -0300

    Codewars ArrayDiff

---
+++ C#/CodeWars/ArrayDiff/Program.cs
@@ -0,0 +1,10 @@
+﻿
+//array has a fixed length
+
+System.Console.WriteLine(ArrayDiff([1, 2, 3], [1,2]));
+ 
+ static int[] ArrayDiff(int[] a, int[] b){
+    //return a when a is not in b array
+    return a.Where(item => !b.Contains(item)).ToArray();
+ }
+
