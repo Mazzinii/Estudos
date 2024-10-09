2e3361310840c08ff0fe4c0f89ce55197736e6ea
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-09 18:45:44 -0300

    CodeWars Replace With Alphabet Position

---
+++ C#/CodeWars/ReplaceWithAlphabetPosition/Program.cs
@@ -0,0 +1,25 @@
+﻿  static string AlphabetPosition(string text)
+  {
+    //tranforming text in uppercase 
+    string textUpper = text.ToUpper();
+    //checking if the text has no words
+    if (text.Length == 0 ){
+        return "";
+    }
+    string result = "";
+    //iterating the text
+    foreach(char ch in textUpper){
+        //checking if the word is betwen 'A' and 'Z' 
+        if(ch >= 'A' && ch <= 'Z'){
+            //calculating the position of the word
+             string value = (ch - 'A' + 1).ToString();
+             result += $"{value} ";
+            
+        }
+    }
+        //removing the final space of the string
+       string finalResult = result.TrimEnd(); 
+       return finalResult;
+  }
+
+  AlphabetPosition("The sunset sets at twelve o' clock.");
