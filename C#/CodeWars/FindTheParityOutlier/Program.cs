ea071178299b8b2d5e352bb798cd47c5e1781594
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-10 12:03:23 -0300

    CodeWars FindtheParityOutlier
    You are given an array (which will have a length of at least 3, but could be very large) containing integers. The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.

---
+++ C#/CodeWars/FindTheParityOutlier/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+static int Find(int[] integers)
+    {
+        //iterating array to know if is entirely odd or even
+        int[] numberEven = integers.Where(x => x % 2 != 0).ToArray();
+        int[] numberOdd = integers.Where(x => x % 2 == 0).ToArray();
+
+        //cheking if the iterating result is equal or less than one 
+        //if so return the outlier 
+        if (numberEven.Length <= 1) return numberEven[0];
+        
+        if (numberOdd.Length <= 1) return numberOdd[0];
+
+        return -1;
+        
+    }
+
+Find([160, 3, 1719, 19, 11, 13, -21]);
+
