38908bed0aabe1a45f0fc61f4fe5e099b93f07b3
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-16 23:58:33 -0300

    CodeWars BinaryAddition

---
+++ C#/CodeWars/BinaryAddition/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+static string AddBinary(int a, int b)
+  {
+   int number = a + b;
+   List<int> binary = new List<int>{};
+   //calculating binary  
+   while(number > 0){
+    //adding each number in list
+     binary.Add(number%2); 
+     number /= 2;   
+   }
+   //reversing the list binary
+    binary.Reverse();
+    string binaryReverse = "";
+   //converting each int on list in string  
+    foreach(int i in binary){
+        binaryReverse += i.ToString();
+    }
+    return binaryReverse;
+  }
+  System.Console.WriteLine(AddBinary(20,5));
+
