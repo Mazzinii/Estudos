2d62910f5169f644c5811e3ba6cd1d5c805e55ff
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-16 23:59:07 -0300

    CodeWars Couting Sheep

---
+++ C#/CodeWars/CountingSheep/Program.cs
@@ -0,0 +1,13 @@
+﻿  static int CountSheeps(bool[] sheeps)
+  {
+    return sheeps.Where(x => x == true).Count();
+  }
+
+System.Console.WriteLine(CountSheeps([true,  true,  true,  false,
+  true,  true,  true,  true ,
+  true,  false, true,  false,
+  true,  false, false, true ,
+  true,  true,  true,  true ,
+  false, false, true,  true]));
+
+  // output : 17
