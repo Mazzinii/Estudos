4b0dc858dde3d922385713c70d88a2c2e156bfe5
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-21 23:25:59 -0300

    Generics, Set, Dictionary (Set)

---
+++ C#/Generics, Set, Dictionary/Set/Program.cs
@@ -0,0 +1,27 @@
+﻿System.Console.Write("How many students for course A?");
+int studentsA = int.Parse(Console.ReadLine());
+
+HashSet<int> hashStudents= new HashSet<int>();
+
+for(int i = 0; i < studentsA; i++){
+    int idStudent = int.Parse(Console.ReadLine());
+    hashStudents.Add(idStudent);
+}
+
+System.Console.Write("How many students for course B?");
+int studentsB = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < studentsB; i++){
+    int idStudent = int.Parse(Console.ReadLine());
+    hashStudents.Add(idStudent);
+}
+
+System.Console.Write("How many students for course C?");
+int studentsC = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < studentsA; i++){
+    int idStudent = int.Parse(Console.ReadLine());
+    hashStudents.Add(idStudent);
+}
+
+System.Console.WriteLine($"Total students: {hashStudents.Count}");
