b2ad9e9eb06874c8d4ffa27efb8c8f2e5e9794ad
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-21 23:23:13 -0300

    CodeWars Playing With Digits

---
+++ C#/CodeWars/PlayingWithDigits/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+static long digPow(int n, int p) {
+	if(p < 0 || n < 0){
+      return -1;
+    }
+    string nStr = n.ToString();
+    long k = 0;
+    foreach(char c in nStr){
+        int digit = (int)char.GetNumericValue(c);
+        k += (int)Math.Pow(digit,p);
+        p++;
+    }
+       int nNum = (int)n;
+       double result = k / nNum;
+       
+       if( result * nNum != k){
+        return -1;
+       }
+       return (long)result;
+       
+}
+System.Console.WriteLine(digPow(92, 1));
+
+
+System.Console.WriteLine(Math.Floor(19.99));
+
+
