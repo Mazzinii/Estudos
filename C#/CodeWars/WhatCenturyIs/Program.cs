dd920cf67519a183dc8d0a24fd2f1c882afbb08f
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-21 23:23:43 -0300

    CodeWars What Century Is

---
+++ C#/CodeWars/WhatCenturyIs/Program.cs
@@ -0,0 +1,22 @@
+﻿   static string WhatCentury(string year)
+  {
+    double yearint = Convert.ToDouble(year);
+    yearint /= 100;
+    string century = Math.Ceiling(yearint).ToString();
+    char finalDigit = century[1];
+    char firstDigit = century[0];
+    
+    if(firstDigit == '1'){
+      return $"{century}th";
+    }
+    if(finalDigit == '1'){
+      return $"{century}st";
+    }  
+    if(finalDigit == '2'){
+      return $"{century}nd";
+    }  
+    if(finalDigit == '3'){
+      return $"{century}rd";
+    }
+    return $"{century}th";
+  }
