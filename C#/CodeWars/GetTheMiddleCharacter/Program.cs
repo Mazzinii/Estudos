a5859efecf2ffc7b78e937d9abf335772dac5232
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-22 21:15:16 -0300

    update with substring

--- C#/CodeWars/GetTheMiddleCharacter/Program.cs
+++ C#/CodeWars/GetTheMiddleCharacter/Program.cs
@@ -1,13 +1,13 @@
 ﻿static string GetMiddle(string s)
   {
-    //checking if the string has one word
-    if(s.Length == 0){
-      return s;
-    }
     //calculating the middle of the string 
     int middle = s.Length / 2;
     //if string has odd length return 2 letters (the middle character and the previous one)
     //if not return the middle word
-    return s.Length % 2 == 0 ? $"{s[middle]}{s[middle -1]}" : s[middle].ToString() ;
+    return s.Length % 2 == 0 ? s.Substring(middle -1, 2) : s[middle].ToString();
   }
+
+  System.Console.WriteLine(GetMiddle("A"));
+
+  
 
