0d32ee8b2340eeeefe64f2cd3b0e11da58167f55
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-24 12:00:29 -0300

    Update switch statement to expression

--- C#/CodeWars/WhoLikesIt/Program.cs
+++ C#/CodeWars/WhoLikesIt/Program.cs
@@ -3,7 +3,7 @@
     switch (name.Length)
     {
         case 0:
-            return $"no one like this";
+            return $"no one likes this";
         case 1:
              return $"{name[0]} likes this";
         case 2:
@@ -12,7 +12,18 @@
             return $"{name[0]}, {name[1]} and {name[2]} like this";     
         default:
             return $"{name[0]}, {name[1]} and {name.Length -2} others like this";
-    }   
-  }
+    }
+    /* or expression
+        
+    return name.Length switch
+    {
+        0 => $"no one likes this",
+        1 => $"{name[0]} likes this",
+        2 => $"{name[0]} and {name[1]} like this",
+        3 => $"{name[0]}, {name[1]} and {name[2]} like this",
+        _ => $"{name[0]}, {name[1]} and {name.Length - 2} others like this",
+    };
+    */
+}
 
   System.Console.WriteLine(Likes(["Alex", "Jacob", "Mark", "Max"]));
