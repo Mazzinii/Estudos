8963caea99e63526d0e45d953650fa9388e45a32
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-24 11:56:56 -0300

    Dictionary

---
+++ C#/Generics, Set, Dictionary/Dictionary/Program.cs
@@ -0,0 +1,36 @@
+﻿
+System.Console.Write("Enter file full path:");
+string path = Console.ReadLine();
+//C:\Users\lmazz\Downloads\CodeGit\Estudos\C#\Generics, Set, Dictionary\Dictionary\in.txt
+
+ Dictionary<string, int> data = new Dictionary<string, int>();
+
+try{
+    using (StreamReader sr = File.OpenText(path)) {
+
+       
+        
+        while(!sr.EndOfStream){
+            //read line and transform in array with sep (,)
+            string[] split = sr.ReadLine().Split(',');
+            string candidate = split[0];
+            int vote = int.Parse(split[1]);
+
+            if(data.ContainsKey(candidate)){
+                data[candidate] += vote;
+            }
+            else{
+                data[candidate] = vote;
+            }
+
+        }    
+    }
+        
+}                  
+catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+
+foreach(var line in data){
+    Console.WriteLine(line.Key + ":" + line.Value);
+}
