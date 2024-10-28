4d5a7c1afe6006483548bf3bddad4c53365cca17
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-28 19:13:16 -0300

    CodeWars Duplicate Encoder
    The goal of this exercise is to convert a string to a new string where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
    
    Examples
    "din"      =>  "((("
    "recede"   =>  "()()()"
    "Success"  =>  ")())())"
    "(( @"     =>  "))(("

---
+++ C#/CodeWars/Fundamentals/DuplicateEncoder/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+static string DuplicateEncode(string word)
+  {
+    //to lower
+    word = word.ToLower();
+    Dictionary<char, int> values = new();
+    //what letters appears more tha once
+    foreach (char ch in word)
+    {
+        //increase count if values has character
+        if (values.ContainsKey(ch)){
+            values[ch]++;
+        }
+        else{
+            values[ch] = 1;
+        }
+    }
+
+    string result = "";
+    for(int i = 0; i < word.Length; i++){
+        
+        result += values[word[i]] == 1 ? '(' : ')';
+    }
+    return result; 
+  } 
+
+  System.Console.WriteLine(DuplicateEncode("recede"));
