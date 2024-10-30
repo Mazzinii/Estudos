16abb0ccb7bea146e1df54ebc4fb91d72c13097f
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-30 11:08:38 -0300

    CodeWars Fundamentals Reversed String
    Complete the solution so that it reverses the string passed into it.
    
    'world'  =>  'dlrow'
    'word'   =>  'drow'

---
+++ C#/CodeWars/Fundamentals/ReversedStrings/Program.cs
@@ -0,0 +1,13 @@
+﻿ static string Solution(string str) 
+  {
+   return new string(str.ToArray().Reverse().ToString());
+  }
+  
+System.Console.WriteLine(Solution("World"));
+
+/*
+str.ToArray(): Converte a string str em um array de caracteres.
+Reverse(): Inverte a ordem dos caracteres.
+ToArray(): Converte o resultado do Reverse() de volta para um array.
+new string(...): Cria uma nova string a partir do array invertido.
+*/
