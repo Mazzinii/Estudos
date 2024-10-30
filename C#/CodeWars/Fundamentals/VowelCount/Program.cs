4e945d4ba0101fea50c3f4df1d92c9ae32abad2e
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-29 21:15:53 -0300

    CodeWars Fundamentals Vowel Count
    Return the number (count) of vowels in the given string.
    
    We will consider a, e, i, o, u as vowels for this Kata (but not y).
    
    The input string will only consist of lower case letters and/or spaces.

---
+++ C#/CodeWars/Fundamentals/VowelCount/Program.cs
@@ -0,0 +1,18 @@
+﻿ static int GetVowelCount(string str)
+    {
+        int vowelCount = 0;
+        List<char> vowel = new List<char>(){'a','e','i','o','u'};
+
+        foreach(char ch in str){
+           if(vowel.Contains(ch)){
+            vowelCount++;
+           } 
+        }
+        return vowelCount;
+
+        /* using Linq
+        return str.Count(i => "aeiou".Contains(i));
+        */
+    }
+
+System.Console.WriteLine(GetVowelCount("abracadabra"));
