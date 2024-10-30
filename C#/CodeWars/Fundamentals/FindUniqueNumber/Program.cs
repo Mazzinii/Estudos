61638c1ab9541411a9134e696b3d7869175f5bbe
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-30 11:07:07 -0300

    CodeWars Fundamentals Find Unique Number
    There is an array with some numbers. All numbers are equal except for one. Try to find it!
    
    findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
    findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
    It’s guaranteed that array contains at least 3 numbers.
    
    The tests contain some very huge arrays, so think about performance.

---
+++ C#/CodeWars/Fundamentals/FindUniqueNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+static int GetUnique(IEnumerable<int> numbers)
+  {
+   List<int> result = new();
+
+   foreach(var number in numbers){
+    if(!result.Contains(number)){
+        result.Add(number);
+    }
+    else{
+        result.Remove(number);
+    }
+   }
+    return result[0];
+    /*
+    return numbers.GroupBy(x=>x).Single(x=> x.Count() == 1).Key
+    */
+  }
+
+  System.Console.WriteLine(GetUnique([ 1, 1, 1, 2, 1, 1 ]));
