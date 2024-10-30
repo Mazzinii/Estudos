22e4facce1fcaedd728bfcd72148dc0b2f9e354a
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-29 21:14:14 -0300

    CodeWars Fundamentals Unique in Order
    Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
    
    For example:
    
    uniqueInOrder("AAAABBBCCDAABBB") == {'A', 'B', 'C', 'D', 'A', 'B'}
    uniqueInOrder("ABBCcAD")   == {'A', 'B', 'C', 'c', 'A', 'D'}
    uniqueInOrder([1,2,2,3,3])     == {1,2,3}

---
+++ C#/CodeWars/Fundamentals/UniqueInOrder/Program.cs
@@ -0,0 +1,21 @@
+﻿
+static IEnumerable<T> UniqueInOrder<T>(IEnumerable<T> iterable) 
+  {
+    var set = new List<T>();
+   
+    foreach(var item in iterable){
+      //checking if the last item is diferent than current item
+      if(item.Equals(set.LastOrDefault())){
+        set.Add(item);
+      }
+        
+    }
+    return set;   
+  }
+  System.Console.WriteLine(UniqueInOrder("ABBCcAD"));
+    
+
+
+
+
+
