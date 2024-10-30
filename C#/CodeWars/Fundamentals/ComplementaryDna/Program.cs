a86aeea03fcc99fec136aa40d3b2ecc303314bd2
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-30 11:05:11 -0300

    CodeWars Fundamentals Complementary DNA
    Deoxyribonucleic acid (DNA) is a chemical found in the nucleus of cells and carries the "instructions" for the development and functioning of living organisms.
    
    If you want to know more: http://en.wikipedia.org/wiki/DNA
    
    In DNA strings, symbols "A" and "T" are complements of each other, as "C" and "G". Your function receives one side of the DNA (string, except for Haskell); you need to return the other complementary side. DNA strand is never empty or there is no DNA at all (again, except for Haskell).
    
    More similar exercise are found here: http://rosalind.info/problems/list-view/ (source)
    
    Example: (input --> output)
    
    "ATTGC" --> "TAACG"
    "GTAT" --> "CATA"

---
+++ C#/CodeWars/Fundamentals/ComplementaryDna/Program.cs
@@ -0,0 +1,23 @@
+﻿ static string MakeComplement(string dna)
+{   
+  string complement = "";
+  foreach(char ch in dna){
+    switch(ch){
+        case 'A':
+            complement += "T";
+            break;    
+        case 'T':
+            complement += "A";
+            break;    
+        case 'G':
+            complement += "C";
+            break;    
+        case 'C':
+            complement += "G";
+            break;    
+    }
+  }
+    return complement;
+}
+
+System.Console.WriteLine(MakeComplement("ATTGC"));
