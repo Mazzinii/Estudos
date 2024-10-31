7d822528e911da4351c1558498612faf7edefac3
Mazzinii <luizeduardomazzini@gmail.com>
2024-10-31 16:54:10 -0300

    Update Program.cs

--- C#/CodeWars/Fundamentals/ReversedStrings/Program.cs
+++ C#/CodeWars/Fundamentals/ReversedStrings/Program.cs
@@ -1,13 +1,13 @@
 ﻿ static string Solution(string str) 
   {
-   return new string(str.ToArray().Reverse().ToString());
+   return new string(str.ToArray().Reverse().ToArray());
   }
   
 System.Console.WriteLine(Solution("World"));
 
 /*
+new string(...): Cria uma nova string a partir do array invertido.
 str.ToArray(): Converte a string str em um array de caracteres.
 Reverse(): Inverte a ordem dos caracteres.
 ToArray(): Converte o resultado do Reverse() de volta para um array.
-new string(...): Cria uma nova string a partir do array invertido.
 */
