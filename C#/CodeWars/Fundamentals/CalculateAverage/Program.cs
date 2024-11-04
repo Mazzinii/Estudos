f88b020b28f4eb862200b2859f67abec0bbd7f94
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-04 20:35:36 -0300

    CodeWars Calculate Average (Fundamentals)
    Write a function which calculates the average of the numbers in a given array.
    
    Note: Empty arrays should return 0.

---
+++ C#/CodeWars/Fundamentals/CalculateAverage/Program.cs
@@ -0,0 +1,6 @@
+﻿ static double FindAverage(double[] array)
+  { 
+   return array.Length == 0 ?  0 : (double)array.Average();
+  }
+
+  FindAverage([17, 16, 16, 16, 16, 15, 17, 17, 15, 5, 17, 17, 16]);
