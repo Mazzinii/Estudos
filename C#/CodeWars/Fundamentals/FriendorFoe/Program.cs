eecb61fe5f56ba5e42b36c6ea09da62ebbac3d5d
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-04 20:38:19 -0300

    CodeWars Friend or Foe (Fundamentals)
    Make a program that filters a list of strings and returns a list with only your friends name in it.
    
    If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
    
    Input = {"Ryan", "Kieran", "Jason", "Yous"}
    Output = {"Ryan", "Yous"}
    
    Input = {"Peter", "Stephen", "Joe"}
    Output = {}
    
    Input strings will only contain letters. Note: keep the original order of the names in the output.

---
+++ C#/CodeWars/Fundamentals/FriendorFoe/Program.cs
@@ -0,0 +1,15 @@
+﻿ static IEnumerable<string> FriendOrFoe (string[] names) {
+   //using LINQ
+   return names.Where(x => x.Length == 4).ToList();
+   
+   /*
+   List<string> nameList = [];
+   foreach(var name in names){
+        if(name.Length == 4){
+            nameList.Add(name);
+        }
+   }
+   return nameList; 
+   */
+  }
+FriendOrFoe(new []{"Ryan", "Kieran", "Jason", "Yous"});
