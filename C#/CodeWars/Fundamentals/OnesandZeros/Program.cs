684bdede7442b17c45b22e357b2993a8aa652e70
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-04 20:43:07 -0300

    CodeWars Ones and Zeros (Fundamentals)
    Given an array of ones and zeroes, convert the equivalent binary value to an integer.
    
    Eg: [0, 0, 0, 1] is treated as 0001 which is the binary representation of 1.
    
    Examples:
    
    Testing: [0, 0, 0, 1] ==> 1
    Testing: [0, 0, 1, 0] ==> 2
    Testing: [0, 1, 0, 1] ==> 5
    Testing: [1, 0, 0, 1] ==> 9
    Testing: [0, 0, 1, 0] ==> 2
    Testing: [0, 1, 1, 0] ==> 6
    Testing: [1, 1, 1, 1] ==> 15
    Testing: [1, 0, 1, 1] ==> 11
    
    However, the arrays can have varying lengths, not just limited to 4.

---
+++ C#/CodeWars/Fundamentals/OnesandZeros/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+static int binaryArrayToNumber(int[] BinaryArray)
+{   
+    string binaryNumber = "";
+    foreach(int binary in BinaryArray) binaryNumber += binary;
+    return Convert.ToInt32(binaryNumber,2);
+}
+System.Console.WriteLine(binaryArrayToNumber([1, 1, 1, 1]));
