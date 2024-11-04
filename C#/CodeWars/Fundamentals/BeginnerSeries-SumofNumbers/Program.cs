9b21fb81e3b8eb1e8ff902144f2b6b547940a9f9
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-04 20:34:08 -0300

    CodeWars Sum of Numbers (Fundamentals)
    Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
    
    Note: a and b are not ordered!
    
    Examples (a, b) --> output (explanation)
    (1, 0) --> 1 (1 + 0 = 1)
    (1, 2) --> 3 (1 + 2 = 3)
    (0, 1) --> 1 (0 + 1 = 1)
    (1, 1) --> 1 (1 since both are same)
    (-1, 0) --> -1 (-1 + 0 = -1)
    (-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2)
    
    Your function should only return a number, not the explanation about how you get that number.

---
+++ C#/CodeWars/Fundamentals/BeginnerSeries-SumofNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int GetSum(int a, int b)
+     {
+       //take min int 
+       int min = Math.Min(a,b);
+       //take max int
+       int max = Math.Max(a,b);
+       //applying the formula 
+       return (max - min + 1 ) * (max + min) / 2;
+
+       /*
+       int result = 0;
+       for (int i = min; i <= max; i++)
+       {       
+         result += i;
+       }
+       return result;
+       */  
+     }
+
+System.Console.WriteLine(GetSum(5,-1));     
