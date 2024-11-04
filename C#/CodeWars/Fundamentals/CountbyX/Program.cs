8627b9551eea147bd96e61f5757bf69c1d7b6704
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-04 20:36:59 -0300

    CodeWars Count by X (Fundamentals)
    Create a function with two arguments that will return an array of the first n multiples of x.
    
    Assume both the given number and the number of times to count will be positive numbers greater than 0.
    
    Return the results as an array or list ( depending on language ).
    
    Examples:
    x = 1, n = 10 --> [1,2,3,4,5,6,7,8,9,10]
    x = 2, n = 5  --> [2,4,6,8,10]

---
+++ C#/CodeWars/Fundamentals/CountbyX/Program.cs
@@ -0,0 +1,16 @@
+﻿ static int[] CountBy(int x, int n)
+  {
+    //know size of vector
+    int arraySize = (x * n) / x;
+    //define pace
+    int pace = x;
+    //start vector
+    int[] z = new int[arraySize];
+    //adding number in vector
+    for(int i = 0; i < arraySize; i++){
+        z[i] = x;
+        x += pace;
+    }
+    return z;
+  }
+CountBy(2,5);
