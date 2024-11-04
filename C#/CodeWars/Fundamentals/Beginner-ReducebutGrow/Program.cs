6a66731256f2c4887f89fd7749a937e22567dba1
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-04 20:31:44 -0300

    CodeWars Reduce but Grow (Fundamentals)
    Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
    
    [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24

---
+++ C#/CodeWars/Fundamentals/Beginner-ReducebutGrow/Program.cs
@@ -0,0 +1,11 @@
+﻿static int Grow(int[] x)
+{
+    int result = 1;
+    foreach(int number in x) result *= number;
+    return result;
+
+    /*Linq
+    return x.Aggregate((a,b) => a*b);
+    */
+}
+System.Console.WriteLine(Grow([1, 2, 3, 4]));
