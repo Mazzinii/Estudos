8ffb88a305c197cb9123476187e8418c87b2efaa
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-06 21:06:11 -0300

    CodeWars Find the Odd Int (Fundamentals)
    Given an array of integers, find the one that appears an odd number of times.
    
    There will always be only one integer that appears an odd number of times.
    
    Examples
    [7] should return 7, because it occurs 1 time (which is odd).
    [0] should return 0, because it occurs 1 time (which is odd).
    [1,1,2] should return 2, because it occurs 1 time (which is odd).
    [0,1,0,1,0] should return 0, because it occurs 3 times (which is odd).
    [1,2,2,3,3,3,4,3,3,3,2,2,1] should return 4, because it appears 1 time (which is odd).

---
+++ C#/CodeWars/Fundamentals/FindtheOddInt/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+static int find_it(int[] seq) 
+      {
+      return seq.GroupBy(x => x) //Group numbers that are the same                      
+                .Where(g => g.Count() % 2 != 0) //Filter Groups that have odd ocurrance
+                .Select(g => g.Key) //Return value of Group
+                .FirstOrDefault();  //Return the first or default value                 
+      }
+
+find_it([0,1,0,1,0]);
