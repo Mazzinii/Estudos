cd70aeefc05c7e943c273f6d71536f03d9df2d75
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-07 23:42:53 -0300

    CodeWars Write Number in Expanded Form
    Write Number in Expanded Form
    You will be given a number and you will need to return it as a string in Expanded Form. For example:
    
    12 --> "10 + 2"
    45 --> "40 + 2"
    70304 --> "70000 + 300 + 4"

---
+++ C#/CodeWars/Fundamentals/WriteNumberinExpandedForm/Program.cs
@@ -0,0 +1,22 @@
+﻿
+static string ExpandedForm(long num) 
+{   //transform num in string
+    string numString = num.ToString();
+    //declaring the beginning of power
+    double power = numString.Length - 1;
+    double number = 0;
+    string result = "";
+    foreach(var n in numString){
+         //trasnform char in double
+         double digit = Convert.ToDouble(n.ToString());
+         //calculate each number
+         number = digit * Math.Pow(10,power);
+         //add to string
+         if(number > 0) result += $"{number} ";
+         //decrease power
+         power--;   
+    }
+    //removing blank space from end and replace space between number with " + "
+    return result.TrimEnd().Replace(" ", " + ");     
+}
+ExpandedForm(12);
