1cad5ce8b965a39620baf4b07fbb401445622237
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-07 23:40:05 -0300

    CodeWars Count the Smiley Faces
    Given an array (arr) as an argument complete the function countSmileys that should return the total number of smiling faces.
    
    Rules for a smiling face:
    
    Each smiley face must contain a valid pair of eyes. Eyes can be marked as : or ;
    A smiley face can have a nose but it does not have to. Valid characters for a nose are - or ~
    Every smiling face must have a smiling mouth that should be marked with either ) or D
    No additional characters are allowed except for those mentioned.
    
    Valid smiley face examples: :) :D ;-D :~)
    Invalid smiley faces: ;( :> :} :]
    
    Example
    countSmileys([':)', ';(', ';}', ':-D']);       // should return 2;
    countSmileys([';D', ':-(', ':-)', ';~)']);     // should return 3;
    countSmileys([';]', ':[', ';*', ':$', ';-D']); // should return 1;

---
+++ C#/CodeWars/Fundamentals/CounttheSmileyFaces/Program.cs
@@ -0,0 +1,18 @@
+﻿ static int CountSmileys(string[] smileys) 
+  {
+    int count = 0;
+    foreach(string smiley in smileys){
+        if(smiley.Length == 3){
+             if((smiley[0] == ':' || smiley[0] == ';') && (smiley[1] == '-' || smiley[1] == '~') && (smiley[2] == ')' || smiley[2] == 'D')) count++;
+        }
+        if(smiley.Length == 2){
+            if((smiley[0] == ':' || smiley[0] == ';') && (smiley[1] == ')' || smiley[1] == 'D')) count++;
+        }
+    }
+    return count;
+
+    /* Using Regex
+        return smileys.Count(s => Regex.IsMatch(s, @"^[:;]{1}[~-]{0,1}[\)D]{1}$"));
+    */
+  }
+CountSmileys([":)", ":(", ":D", ":O", ":;"]);
