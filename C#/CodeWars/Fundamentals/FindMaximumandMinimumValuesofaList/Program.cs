96a3064a4ecd09ed014e62a4c6797b98b9a2a04b
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-11 22:50:17 -0300

    CodeWars Find Maximum and Minimum Values of a List (fundamentals)

---
+++ C#/CodeWars/Fundamentals/FindMaximumandMinimumValuesofaList/Program.cs
@@ -0,0 +1,13 @@
+﻿  static int Min(int[] list)
+  {
+    return list.Min();
+  }
+  
+  static int Max(int[] list)
+  {
+    return list.Max();
+  }
+
+
+Min([-52, 56, 30, 29, -54, 0, -110]);
+Max([-52, 56, 30, 29, -54, 0, -110]);
