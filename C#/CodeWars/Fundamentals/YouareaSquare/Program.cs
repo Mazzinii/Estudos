281c4b258793bd23bac08d88a1d738eef92e362b
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-11 22:51:22 -0300

    CodeWars You are a Square (Fundamentals)
    A square of squares
    You like building blocks. You especially like building blocks that are squares. And what you even like more, is to arrange them into a square of square building blocks!
    
    However, sometimes, you can't arrange them into a square. Instead, you end up with an ordinary rectangle! Those blasted things! If you just had a way to know, whether you're currently working in vain… Wait! That's it! You just have to check if your number of building blocks is a perfect square.
    
    Task
    Given an integral number, determine if it's a square number:
    
    In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.

---
+++ C#/CodeWars/Fundamentals/YouareaSquare/Program.cs
@@ -0,0 +1,10 @@
+﻿static bool IsSquare(int n)
+  {
+    double square = Math.Sqrt(n);
+    return Math.Floor(square) != square || n < 0 ? false : true;
+
+    /*OR(verifica se o numero tem ou nao parte decimal caso haja o valor será diferente de 0)
+    return Math.Floor(n) % 1 == 0;
+    */
+  }
+  IsSquare(4);
