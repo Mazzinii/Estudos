fa709afffec356683867b329c806d5b860b7accb
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-13 19:13:03 -0300

    CodeWars Build Tower (Fundamentals)
    Build Tower
    Build a pyramid-shaped tower, as an array/list of strings, given a positive integer number of floors. A tower block is represented with "*" character.
    
    For example, a tower with 3 floors looks like this:
    
    [
      "  *  ",
      " *** ",
      "*****"
    ]
    
    And a tower with 6 floors looks like this:
    
    [
      "     *     ",
      "    ***    ",
      "   *****   ",
      "  *******  ",
      " ********* ",
      "***********"
    ]

---
+++ C#/CodeWars/Fundamentals/BuildTower/Program.cs
@@ -0,0 +1,15 @@
+﻿ static string[] TowerBuilder(int nFloors)
+  {
+    string[] tower = new string[nFloors];
+    int quant = 1;
+    int quantSpace = nFloors - 1;
+    for(int i = 0; i < nFloors; i++){
+        string caracter = new string('*', quant);
+        string space = new string(' ', quantSpace);
+        tower[i] = space + caracter + space;
+        quantSpace--;
+        quant += 2;
+      }
+    return tower;
+  }
+  TowerBuilder(6);
