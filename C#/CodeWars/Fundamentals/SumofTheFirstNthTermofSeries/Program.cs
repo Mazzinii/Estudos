12b831ea3a2809225f5d8a7253ce90b4acb982ba
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-12 23:19:01 -0300

    CodeWars Sum of the first nth term of Series (Fundamentals)
    Task
    Your task is to write a function which returns the n-th term of the following series, which is the sum of the first n terms of the sequence (n is the input parameter).
    
    You will need to figure out the rule of the series to complete this.
    
    Rules
    You need to round the answer to 2 decimal places and return it as String.
    
    If the given value is 0 then it should return "0.00".
    
    You will only be given Natural Numbers as arguments.
    
    Examples (Input --> Output)
    n
    1 --> 1 --> "1.00"
    2 --> 1 + 1/4 --> "1.25"
    5 --> 1 + 1/4 + 1/7 + 1/10 + 1/13 --> "1.57"

---
+++ C#/CodeWars/Fundamentals/SumofTheFirstNthTermofSeries/Program.cs
@@ -0,0 +1,10 @@
+﻿ static string seriesSum (int n) {
+    double result = 0;
+    double denominador = 1;
+    for(int i = 1; i <= n; i++){
+      result += 1 / denominador;
+      denominador += 3;
+    }
+    return result.ToString("0.00");
+ }
+ seriesSum(9);
