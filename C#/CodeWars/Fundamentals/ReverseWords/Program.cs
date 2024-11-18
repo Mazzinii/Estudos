b4fd8f124cf71a7aff2d1ce14b267fe5be835f7d
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-18 19:07:01 -0300

    CodeWars Reverse Words(Fundamentals)
    Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
    
    Examples
    "This is an example!" ==> "sihT si na !elpmaxe"
    "double  spaces"      ==> "elbuod  secaps"

---
+++ C#/CodeWars/Fundamentals/ReverseWords/Program.cs
@@ -0,0 +1,13 @@
+﻿static string ReverseWords(string str)
+{
+    string[] word = new string(str.ToArray().Reverse().ToArray()).Split(" "); 
+    return string.Join(" ", word.Reverse());
+
+    /*Linq
+    return string.Join(" ", str.Split(' ').Select(i => new string(i.Reverse().ToArray())));
+    */
+
+  
+}
+
+ReverseWords("This is an example!");
