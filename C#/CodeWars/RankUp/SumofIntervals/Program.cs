82bce2f68ae4ad0ee32c56f71967502b6f12ac7f
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-18 19:11:57 -0300

    CodeWars Sum of Intervals (RankUp)
    Write a function called sumIntervals/sum_intervals that accepts an array of intervals, and returns the sum of all the interval lengths. Overlapping intervals should only be counted once.
    
    Intervals
    Intervals are represented by a pair of integers in the form of an array. The first value of the interval will always be less than the second value. Interval example: [1, 5] is an interval from 1 to 5. The length of this interval is 4.
    
    Overlapping Intervals
    List containing overlapping intervals:
    
    [
       [1, 4],
       [7, 10],
       [3, 5]
    ]
    The sum of the lengths of these intervals is 7. Since [1, 4] and [3, 5] overlap, we can treat the interval as [1, 5], which has a length of 4.
    
    Examples:
    sumIntervals( [
       [1, 2],
       [6, 10],
       [11, 15]
    ] ) => 9
    
    sumIntervals( [
       [1, 4],
       [7, 10],
       [3, 5]
    ] ) => 7
    
    sumIntervals( [
       [1, 5],
       [10, 20],
       [1, 6],
       [16, 19],
       [5, 11]
    ] ) => 19
    
    sumIntervals( [
       [0, 20],
       [-100000000, 10],
       [30, 40]
    ] ) => 100000030
    Tests with large intervals
    Your algorithm should be able to handle large intervals. All tested intervals are subsets of the range [-1000000000, 1000000000].

---
+++ C#/CodeWars/RankUp/SumofIntervals/Program.cs
@@ -0,0 +1,79 @@
+﻿
+
+static int SumIntervals((int, int)[] intervals)
+    {
+        Array.Sort(intervals, (x,y) => x.Item1 - y.Item1);
+        int max = int.MinValue;
+        int total = 0;
+        foreach (var interval in intervals) 
+        {
+            max = Math.Max(max, interval.Item1);
+            total += Math.Max(0, interval.Item2 - max);
+            max = Math.Max(max, interval.Item2);
+        }
+        return total;
+    }
+
+
+SumIntervals([(1, 5), (10, 20), (1, 6), (16, 19), (5, 11)]);    
+
+/*
+// Ordena os intervalos com base no início de cada intervalo
+        var sortedIntervals = intervals.OrderBy(interval => interval.Item1).ToList();
+
+        List<(int, int)> mergedIntervals = new List<(int, int)>();
+
+        foreach (var interval in sortedIntervals)
+        {
+            // Se a lista de intervalos mesclados estiver vazia ou o intervalo atual não se sobrepõe ao último intervalo
+            if (mergedIntervals.Count == 0 || mergedIntervals.Last().Item2 < interval.Item1)
+            {
+                // Não há sobreposição, apenas adiciona o intervalo
+                mergedIntervals.Add(interval);
+            }
+            else
+            {
+                // Há sobreposição, mescla os intervalos
+                var last = mergedIntervals.Last();
+                mergedIntervals[mergedIntervals.Count - 1] = (last.Item1, Math.Max(last.Item2, interval.Item2));
+            }
+        }
+
+        // Soma os comprimentos dos intervalos mesclados
+        int result = 0;
+        foreach (var interval in mergedIntervals)
+        {
+            result += interval.Item2 - interval.Item1;
+        }
+
+        return result;
+    }
+*/
+
+/*
+int result = 0; 
+       for(int i = 0; i < intervals.Length; i++){
+            if(intervals[i].Item1 == intervals[i].Item2) result+= 0;
+            else{
+                if(intervals[i].Item1 < intervals[i].Item2)
+                result += - intervals[i].Item1 + intervals[i].Item2;
+                else result += intervals[i].Item1 + intervals[i].Item2;
+            }
+       }
+       return result;
+*/       
+
+
+/*
+       int result = 0; 
+       if(intervals[0].Item1 == intervals[0].Item2) result = 0;
+       List<int> count = new List<int>();
+       for(int i = 0; i < intervals.Length; i++ ){
+        while(intervals[i].Item1 != intervals[i].Item2){
+            intervals[i].Item1 = intervals[i].Item1 + 1;
+            count.Add(intervals[i].Item1);
+        }
+       }
+       result = count.Distinct().Count();
+       return result;
+*/
