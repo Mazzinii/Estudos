c8a92887a660a06f726bf716383f00dfcefc4221
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-19 23:57:58 -0300

    CodeWars Parse Char to Int

---
+++ C#/CodeWars/Fundamentals/ParseNiceIntfromChar/Program.cs
@@ -0,0 +1,8 @@
+﻿ static int GetAge(string inputString)
+  {
+    return int.Parse(inputString.Substring(0, 1));
+    
+    /*subtraction from 0 returns a int
+    return inputString[0] - '0';
+    */
+  }
