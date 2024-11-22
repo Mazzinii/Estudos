e7ce17fa022bbdf93f47312ebde833de3b5dad03
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-22 10:19:10 -0300

    CodeWars Mexican Wave(Fundamentals)
    In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up.
    Rules
     1.  The input string will always be lower case but maybe empty.
    
     2.  If the character in the string is whitespace then pass over it as if it was an empty seat
    Example
    wave("hello") => {"Hello", "hEllo", "heLlo", "helLo", "hellO"}

---
+++ C#/CodeWars/Fundamentals/MexicanWave/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+static List<string> wave(string str)
+{
+   List<string> result = new List<string>();
+   int count = 0;
+
+   foreach(char ch in str){  
+    result.Add(str.Remove(count, 1).Insert(count,char.ToUpper(ch).ToString()));
+    count++;
+   }
+   result.RemoveAll(x => x == str);
+   return result;
+}
+
+wave(" gap ");
+
