ae920b59a72c3d05d6303d9c618a89913786aad4
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-22 20:43:43 -0300

    CodeWars Your Order Please(Fundamentals)
    Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
    
    Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
    
    If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
    
    Examples
    "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
    "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
    ""  -->  ""

---
+++ C#/CodeWars/Fundamentals/YourOrderPlease/Program.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+using System.Security.Cryptography.X509Certificates;
+
+static string Order(string words)
+  {
+    return string.Join(" ", words.Split(" ").OrderBy(word => word.ToList().FirstOrDefault(c => Char.IsDigit(c))));
+  }
+  Order("is2 Thi1s T4est 3a");
