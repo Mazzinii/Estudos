865d606595a89660e9a1d008df51a2d3562343ec
Mazzinii <luizeduardomazzini@gmail.com>
2024-11-26 15:55:49 -0300

    CodeWars Adding big Numbers (RankUp)
    We need to sum big numbers and we require your help.
    
    Write a function that returns the sum of two numbers. The input numbers are strings and the function must return a string.
    
    Example
    add("123", "321"); -> "444"
    add("11", "99");   -> "110"
    Notes
    The input numbers are big.
    The input is a string of only digits
    The numbers are positives

---
+++ C#/CodeWars/RankUp/AddingBigNumbers/Program.cs
@@ -0,0 +1,6 @@
+﻿using System.Numerics;
+   static string Add(string a, string b)
+  {
+    return (BigInteger.Parse(a) + BigInteger.Parse(b)).ToString(); 
+  }
+Add("1057853509440367665682450458794866464501746580388666517943654", "1057853509440367665682450458794866464501746580388666517943654");
