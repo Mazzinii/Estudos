b452d0bb95f017d24206bf57d667e71440361434
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-03 19:19:15 -0300

    HackerRank PlusMinus (Interview Preparation)
    Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal
    
    value of each fraction on a new line with 6 places after the decimal.
    
    Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers
    with absolute error of up to 104 are acceptable.
    
    Example
    arr = [1,1,0,-1,-1]
    There are n = 5 elements, two positive, two negative and one zero. Their ratios are 2 / 5 = 0.400000. 2 / 5= 0.400000 and
    1 / 5= 0.200000. Results are printed as:
    
    0.400000
    0.400000
    0.200000

---
+++ C#/HackerRank/Interview Preparation/PlusMinus/Program.cs
@@ -0,0 +1,36 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+       int length = arr.Count();   
+       float positiveNumbers = arr.Where(x => x > 0).Count() ;
+       float negativeNumbers = arr.Where(x => x < 0).Count() ;
+       float zeroNumbers = arr.Where(x => x == 0).Count() ;
+       
+       Console.WriteLine((positiveNumbers / length).ToString("f6"));
+       Console.WriteLine((negativeNumbers / length).ToString("f6"));
+       Console.WriteLine((zeroNumbers / length).ToString("f6"));
+         
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        Result.plusMinus(arr);
+    }
+}
+
