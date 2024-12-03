3666431f0b6024f408ab8204bfe2e05193778c9d
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-03 00:50:21 -0300

    CodeWars Break CamelCase (Fundamentals)
    Complete the solution so that the function will break up camel casing, using a space between words.
    
    Example
    "camelCasing"  =>  "camel Casing"
    "identifier"   =>  "identifier"
    ""             =>  ""

---
+++ C#/CodeWars/Fundamentals/BreakCamelCase/Program.cs
@@ -0,0 +1,17 @@
+﻿ using System.Text.RegularExpressions;
+ static string BreakCamelCase(string str)
+  {
+    string[] vect = Regex.Split(str, "(?=[A-Z])");
+    string result = "";
+
+    foreach(string vectStr in vect){
+      result += $" {vectStr}";
+    }
+    return result.TrimStart();
+
+    /* Linq
+    return String.Concat(str.Select(c => char.IsUpper(c) ? "" + c : c.ToString()));
+    */
+  }
+
+BreakCamelCase("camelCasing");
