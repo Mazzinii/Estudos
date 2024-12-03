3ac3c6672f404b8fd0f8f7ad753afd7be1d31ae0
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-03 00:47:42 -0300

    CodeWars Maximum Subarray Sum (Fundamentals)
    The maximum sum subarray problem consists in finding the maximum sum of a contiguous subsequence in an array or list of integers:
    
    maxSequence [-2, 1, -3, 4, -1, 2, 1, -5, 4]
    -- should be 6: [4, -1, 2, 1]
    Easy case is when the list is made up of only positive numbers and the maximum sum is the sum of the whole array. If the list is made up of only negative numbers, return 0 instead.
    
    Empty list is considered to have zero greatest sum. Note that the empty list or array is also a valid sublist/subarray.

---
+++ C#/CodeWars/Fundamentals/MaximumSubarraySum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Globalization;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        static int MaxSequence(int[] arr)
+        {
+            
+            int max_actual = 0;
+            int max_global = 0;
+
+            foreach (int item in arr)
+            {
+                max_actual = Math.Max(item, item + max_actual);
+                max_global = Math.Max(max_actual, max_global);
+            }
+            return max_global;
+
+        }
+
+        Console.WriteLine(MaxSequence(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
+    }
+}
