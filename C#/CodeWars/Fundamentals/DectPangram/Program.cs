f34f07fe7c486e279b1ca5f0230b566a1d974436
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-03 22:35:50 -0300

    CodeWars Dect Pangram (Fundamentals)
    A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
    
    Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.

---
+++ C#/CodeWars/Fundamentals/DectPangram/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.RegularExpressions;
+
+static bool IsPangram(string str)
+  {
+    SortedSet<char> result = new SortedSet<char>();
+    // replace whith " " if is not a letter
+    string newText = Regex.Replace(str.ToLower(), @"[^a-z]", "");
+    // adding in SortedSet
+    foreach(char c in newText){
+        result.Add(c);
+    }
+    return result.Count == 26;
+
+    /* Linq
+
+    return str.Where(ch => Char.IsLetter(ch)).Select(ch => Char.ToLower(ch)).Distinct().Count() == 26;
+
+    */
+  }
+
+  IsPangram("The quick brown fox jumps over the lazy dog.");
