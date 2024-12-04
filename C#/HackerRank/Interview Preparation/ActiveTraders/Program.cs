f91f859ab7d03a68cc4da175465b169e3e63e2fc
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-04 15:24:50 -0300

    HackRank
    HackerRank Problem Solving (Basic) Skills Certification Test

---
+++ C#/HackerRank/Interview Preparation/ActiveTraders/Program.cs
@@ -0,0 +1,34 @@
+﻿ static List<string> mostActive(List<string> customers)
+    {
+        double maxOcurrance = customers.Count * 0.05;
+        var names = customers.Distinct();
+        List<string> result = [];
+        
+        foreach(string name in names){
+            if(customers.Count(x => x == name) >= maxOcurrance){
+                result.Add(name);
+            }
+        }
+        result.Sort();
+        return result;
+
+        /*Linq
+        var result = customers
+            .GroupBy(name => name)
+            .Where(group => group.Count() >= maxOcurrance) // Filtra os que atendem ao limite
+            .Select(group => group.Key) // Seleciona apenas os nomes
+            .OrderBy(name => name) // Ordena em ordem alfabética
+            .ToList();
+
+        return result;
+        */
+        
+    }
+
+mostActive(["Bigcorp", "Bigcorp", "Acme", "Bigcorp", "Zork", "Zork",
+"Abc", "Bigcorp", "Acme", "Bigcorp", "Bigcorp", "Zork", "Bigcorp",
+"Zork", "Zork", "Bigcorp", "Acme", "Bigcorp", "Acme", "Bigcorp",
+"Acme", "Littlecorp", "Nadircorp"]);    
+
+
+
