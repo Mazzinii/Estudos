605662248e1412f6cbd05c9d4ab32041105488b2
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-04 15:22:34 -0300

    CodeWars Count Characters in Your String (Fundamentals)
    The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
    
    What if the string is empty? Then the result should be empty object literal, {}.

---
+++ C#/CodeWars/Fundamentals/CountCharactersinYourString/Program.cs
@@ -0,0 +1,16 @@
+﻿ static Dictionary<char, int> Count(string str)
+  {
+    Dictionary<char, int> dict = new Dictionary<char,int>();
+    foreach(char s in str){
+        if(!dict.ContainsKey(s)){
+            dict.Add(s,str.Count(x => x == s));
+        }    
+    }
+    return dict;
+
+    /* Linq
+    return str.GroupBy(c => c).ToDictionary(g => g.Key, g => g.Count());
+    */
+  }
+
+  Count("aba");
