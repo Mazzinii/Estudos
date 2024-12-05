0aed590b48da3c876db01b5eb9734e1b10940f48
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-05 20:54:59 -0300

    HackerRank Time Conversion (Interview Preparation )

---
+++ C#/HackerRank/Interview Preparation/TimeConversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+class Result
+{
+
+    /*
+     * Complete the 'timeConversion' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static void timeConversion(string s)
+    {
+        DateTime hour = DateTime.ParseExact(s, "hh:mm:sstt", CultureInfo.InvariantCulture);
+        Console.WriteLine($"{hour.ToString("HH:mm:ss")}");
+         
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+            timeConversion("07:05:45PM");
+    }
+}
+
+}
+
