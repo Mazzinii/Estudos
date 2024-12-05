2d4fa5e9be843bd1ee639a86913f6ff44869d386
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-05 20:54:13 -0300

    HackerRank Min and Max (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/MinMaxSum/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Numerics;
+class Result
+{
+
+    /*
+     * Complete the 'miniMaxSum' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void miniMaxSum(List<int> arr)
+    {
+        
+        arr.Sort();
+        long min = (long)arr[0] + arr[1] + arr[2] + arr[3];
+        long max = (long)arr[1]+ arr[2] + arr[3] + arr[4];
+        
+
+        Console.WriteLine($"{min} {max}");
+    }
+
+    
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+
+            miniMaxSum([140638725, 436257910, 953274816, 734065819, 362748590]);
+        }
+    }
+
+}
