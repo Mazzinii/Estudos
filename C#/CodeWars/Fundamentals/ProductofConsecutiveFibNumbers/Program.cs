8f440f882655c0009c9d015c41e48e17cf7eac6d
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-04 22:47:21 -0300

    CodeWar Product of Consecutive Fib Numbers (Fundamentals)
    The Fibonacci numbers are the numbers in the following integer sequence (Fn): 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
    
    Your function takes an integer (prod) and returns an array/tuple (check the function signature/sample tests for the return type in your language):
    
    if F(n) * F(n+1) = prod:
    (F(n), F(n+1), true)
    If you do not find two consecutive F(n) verifying F(n) * F(n+1) = prod:
    (F(n), F(n+1), false)
    where F(n) is the smallest one such as F(n) * F(n+1) > prod.
    Examples:
    714 ---> (21, 34, true)
    --> since F(8) = 21, F(9) = 34 and 714 = 21 * 34
    
    800 --->  (34, 55, false)
    --> since F(8) = 21, F(9) = 34, F(10) = 55 and 21 * 34 < 800 < 34 * 55

---
+++ C#/CodeWars/Fundamentals/ProductofConsecutiveFibNumbers/Program.cs
@@ -0,0 +1,14 @@
+﻿static ulong[] productFib(ulong prod) {
+        
+    ulong a = 1, b = 1;
+        
+    while (a * b < prod)
+    {  
+        ulong fibonacci = a + b;
+        a = b;
+        b = fibonacci;
+    }
+    return new ulong[] {a ,b ,(a * b == prod) ? 1UL : 0UL};
+}
+
+productFib(714);
