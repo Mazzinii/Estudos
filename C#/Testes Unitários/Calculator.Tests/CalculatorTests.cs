0448bc7166680136448a84537dbe28a3adeb2300
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-10 09:49:12 -0300

    Praticando Testes (xUnit)

---
+++ C#/Testes Unitários/Calculator.Tests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Calculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact(DisplayName = "Given valid numbers, when sum then should success ")]
+        //Given_When_Then
+        public void GivenValidNumbers_WhenSum_ThenShoulSuccess()
+        {
+            //arrange
+            const int resultExpect = 5;
+            const int firstNumber = 3;
+            const int secondNumber = 2;
+
+            //act
+            var resultActual = Calculator.Sum(firstNumber, secondNumber);
+
+            //assert
+            Assert.Equal(resultExpect, resultActual);
+
+        }
+    }
+}
---
+++ C#/Testes Unitários/Calculator/Calculator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public static int Sum(int firstNumber, int secondNumber)
+        {
+            if (firstNumber < 0 || secondNumber < 0)
+            {
+                return -1;
+            }
+
+            return firstNumber + secondNumber;
+        }
+    }
+}
