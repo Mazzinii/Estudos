06639554d8769578de12cca94e823a97dcc35863
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-11 11:17:41 -0300

    HackerRank Sparse Arrays (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/SparseArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'matchingStrings' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. STRING_ARRAY strings
+     *  2. STRING_ARRAY queries
+     */
+
+    public static List<int> matchingStrings(List<string> strings, List<string> queries)
+    {
+        List<int> result = new List<int>();
+
+        foreach (string s in queries) 
+        {
+            if (strings.Contains(s))
+            {
+                result.Add(strings.Count(x => x == s));
+            }
+            else result.Add(0);
+        }
+
+        return result;
+
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+            matchingStrings(["ab", "ab", "abc"], ["ab", "abc", "bc"]);
+    }
+}
+
+}
+
