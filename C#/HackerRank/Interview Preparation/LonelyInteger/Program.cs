c9bef3afe19e00e60f4dbb5af766d0223174edd2
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-11 22:21:44 -0300

    HackerRank Lonely Integer (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/LonelyInteger/Program.cs
@@ -0,0 +1,16 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'lonelyinteger' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int lonelyinteger(List<int> a)
+    {
+        return a.GroupBy(x => x).Single(x => x.Count() == 1).Key;
+    }
+
+}
