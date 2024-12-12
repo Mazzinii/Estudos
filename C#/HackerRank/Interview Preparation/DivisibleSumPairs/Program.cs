4c5ec7070df6657fd28dde46fc03cf6deb08933c
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-11 22:23:55 -0300

    HackerRank Divisible Sum Pairs (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/DivisibleSumPairs/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.Design;
+
+class Result
+{
+
+    /*
+     * Complete the 'divisibleSumPairs' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER k
+     *  3. INTEGER_ARRAY ar
+     */
+
+    public static int divisibleSumPairs(int n, int k, List<int> ar)
+    {
+        int result = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i < j && (ar[i] + ar[j]) % k == 0) result++;
+
+            }
+        }
+
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        divisibleSumPairs(6, 5, [1, 2, 3, 4, 5, 6]);
+    }    
+}
