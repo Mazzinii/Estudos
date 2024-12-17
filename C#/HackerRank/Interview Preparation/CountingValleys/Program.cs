5bd87d11b0a035add0a29e86eb021739e0274f72
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-17 16:06:58 -0300

    HackerRank Counting Valleys (Interview Preparation))

---
+++ C#/HackerRank/Interview Preparation/CountingValleys/Program.cs
@@ -0,0 +1,41 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'countingValleys' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER steps
+     *  2. STRING path
+     */
+
+    public static int countingValleys(int steps, string path)
+    {
+        int result = 0;
+        int count = 0;
+      
+        foreach(char p in path) 
+        {
+            if (p == 'D') count--;
+
+            if (p == 'U')
+            {
+                count++;
+                if (count == 0) result++;
+            }
+            
+        }
+
+        return result;
+    }
+class Solution
+{
+    public static void Main(string[] args)
+    {
+            countingValleys(10, "DDUDDUUDUU");
+    }
+}
+
+}
+
