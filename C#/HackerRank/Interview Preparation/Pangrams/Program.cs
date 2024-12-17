ba879a2c3acacf180b35acaba974e6658b2cd105
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-17 16:10:56 -0300

    HackerRank Pangram (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/Pangrams/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+class Result
+{
+
+    /*
+     * Complete the 'pangrams' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static string pangrams(string s)
+    {
+        SortedSet<char> result = new SortedSet<char>();
+
+        string word = Regex.Replace(s.ToLower(), @"[^a-z]", "");
+
+        foreach (char ch in word)
+        {
+            result.Add(ch);
+        }
+
+        return result.Count == 26 ? "pangram" : "no pangram";
+
+    }
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+}
+
+}
