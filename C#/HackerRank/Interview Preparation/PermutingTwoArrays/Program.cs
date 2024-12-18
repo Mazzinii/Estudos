1f27c4b538746284a0ff8a455ba44f7d83f65d80
Mazzinii <luizeduardomazzini@gmail.com>
2024-12-17 22:56:30 -0300

    HackerRank Permuting Two Arrays (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/PermutingTwoArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'twoArrays' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY A
+     *  3. INTEGER_ARRAY B
+     */
+
+    public static string twoArrays(int k, List<int> A, List<int> B)
+    {
+        A.Sort();
+        B.Sort();
+        B.Reverse();
+        string result = "YES";
+        int count = 0;
+
+        foreach (int number in A)
+        {
+            if ((number + B[count]) < k) result = "NO";
+            count++;
+        }
+        return result;
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+            twoArrays(5, [1, 2, 2, 1], [3, 3, 3, 4]);
+    }
+}
+
+}
+
