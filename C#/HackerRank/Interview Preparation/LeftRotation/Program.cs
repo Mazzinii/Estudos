97d41039e5993756cae6885ba69600560e7dcb25
Mazzinii <luizeduardomazzini@gmail.com>
2025-01-13 20:39:04 -0300

    HackerRank Left Rotation (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/LeftRotation/Program.cs
@@ -0,0 +1,37 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'rotateLeft' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER d
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static List<int> rotateLeft(int d, List<int> arr)
+    {
+        List<int> result = new List<int>();
+        int count = 0;
+        while(count < d)
+        {
+            int temp = arr[0];
+            arr.Remove(arr[0]);
+            result = arr;
+            result.Add(temp);
+            count++;
+        }
+        return result;
+        
+    }
+class Solution
+{
+    public static void Main(string[] args)
+    {
+            rotateLeft(2,[1, 2, 3, 4, 5]);
+    }
+}
+
+
+}
