5c95f8bbb0bdb0a8695a31cc6bc88f02f1a5742f
Mazzinii <luizeduardomazzini@gmail.com>
2025-01-17 21:24:53 -0300

    Learning about ASP.NET

---
+++ ASP.NET/MVC/Models/Client.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace MVC.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string Email { get; set; }
+        public string? Password { get; set; }
+        public DateTime BornDate { get; set; }
+
+
+    }
+}
