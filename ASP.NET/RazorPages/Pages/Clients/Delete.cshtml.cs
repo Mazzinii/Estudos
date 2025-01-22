f2c3badb9898c2eeb05181053a9d6dfcacd482e7
Mazzinii <luizeduardomazzini@gmail.com>
2025-01-21 22:55:06 -0300

    ASP.NET Razor Pages

---
+++ ASP.NET/RazorPages/Pages/Clients/Delete.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPages.Data;
+using RazorPages.Models;
+
+namespace RazorPages.Pages.Clients
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly RazorPages.Data.RazorPagesContext _context;
+
+        public DeleteModel(RazorPages.Data.RazorPagesContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Client Client { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Client.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (client is not null)
+            {
+                Client = client;
+
+                return Page();
+            }
+
+            return NotFound();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var client = await _context.Client.FindAsync(id);
+            if (client != null)
+            {
+                Client = client;
+                _context.Client.Remove(Client);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
