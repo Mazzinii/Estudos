20b5503e44aad841bd4d0f94b0f0e05c8ad45227
Mazzinii <luizeduardomazzini@gmail.com>
2025-01-27 23:27:44 -0300

    HackerRank Tower Breakers (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/TowerBreakers/Program.cs
@@ -0,0 +1,25 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'towerBreakers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER m
+     */
+
+    public static int towerBreakers(int n, int m)
+    {
+        if (n == 1) return 1;
+        if (m == 1) return 2;
+
+    }
+
+    public static void Main(string[] args)
+    {
+
+        towerBreakers(100000, 1);
+    }
+}
