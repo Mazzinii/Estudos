5ae6a5b34c1857ab3a89e32cfc5163c8759ae3fc
Mazzinii <luizeduardomazzini@gmail.com>
2025-01-29 20:19:28 -0300

    HackerRank MaxMin (InterviewPreparation)

---
+++ C#/HackerRank/Interview Preparation/MaxMin/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Diagnostics;
+
+class Result
+{
+
+    /*
+     * Complete the 'maxMin' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER k
+     *  2. INTEGER_ARRAY arr
+     */
+
+    public static int maxMin(int k, List<int> arr)
+    {
+        Stopwatch stopWatch = new Stopwatch();
+        stopWatch.Start();
+
+        arr.Sort();
+        int calc = 0;
+        int min = int.MaxValue;
+
+        for(int i = 0; i <= arr.Count - k; i++)
+        {
+            calc = arr[i + k - 1] - arr[i];
+
+            if (calc < min) min = calc;
+           
+        }
+
+        stopWatch.Stop();
+
+        return min;
+
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+            maxMin(3, [10, 100, 300, 200,1000,20,30]);
+    }
+}
+
+}
