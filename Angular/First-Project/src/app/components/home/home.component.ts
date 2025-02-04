649455213d9db6224c603aa2d6fd14aa0bce4604
Mazzinii <luizeduardomazzini@gmail.com>
2025-02-03 23:40:42 -0300

    learning about Angular

--- Angular/First-Project/src/app/components/home/home.component.ts
+++ Angular/First-Project/src/app/components/home/home.component.ts
@@ -4,8 +4,20 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
+  //var que podem ser acessados dentro do ambiente HomeComponent
+  name = 'Luiz Eduardo';
+  idButton = 'Login';
+  mustShowTitle = false;
+  listItems = ['item1', 'item2', 'item3', 'item4'];
 
+  submit() {
+    console.log('CLicado');
+  }
+
+  returnEvent(event: any) {
+    console.log(event);
+  }
 }
