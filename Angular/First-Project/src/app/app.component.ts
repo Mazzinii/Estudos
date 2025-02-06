55db7c1ca58b60a271f4ce8c809daf82dffa18ec
Mazzinii <luizeduardomazzini@gmail.com>
2025-02-05 21:14:18 -0300

    learning Angular

--- Angular/First-Project/src/app/app.component.ts
+++ Angular/First-Project/src/app/app.component.ts
@@ -10,4 +10,8 @@ import { HomeComponent } from './components/home/home.component';
 })
 export class AppComponent {
   title = 'First-Project';
+
+  logar(event: string) {
+    console.log(event);
+  }
 }
--- Angular/First-Project/src/app/components/home/home.component.ts
+++ Angular/First-Project/src/app/components/home/home.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  inject,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
+import { SubmitFormService } from '../../services/submit-form.service';
 
 @Component({
   selector: 'app-home',
@@ -8,16 +16,28 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
   //var que podem ser acessados dentro do ambiente HomeComponent
-  name = 'Luiz Eduardo';
+  name = 'Luiz Eduardo Mazzini';
   idButton = 'Login';
   mustShowTitle = false;
   listItems = ['item1', 'item2', 'item3', 'item4'];
 
-  submit() {
-    console.log('CLicado');
-  }
+  //instanciando service para ser usado nesse ambiente
+  private SubmitService = inject(SubmitFormService);
+
+  //recebendo o valor de fora
+  @Input('name') propsDeFora!: string;
+
+  //enviando valor para fora
+  @Output() emitindoValorName = new EventEmitter<string>();
 
   returnEvent(event: any) {
     console.log(event);
   }
+
+  submit() {
+    this.emitindoValorName.emit(this.name);
+    this.SubmitService.SubmitForm();
+  }
+
+  
 }
