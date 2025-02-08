4646343a8502b17cb46602925b98f6d1a703a444
Mazzinii <luizeduardomazzini@gmail.com>
2025-02-07 22:56:26 -0300

    HackerRank Dynamic Array (Interview Preparation)

---
+++ C#/HackerRank/Interview Preparation/DynamicArray/Program.cs
@@ -0,0 +1,31 @@
+﻿class Result
+{
+
+    /*
+     * Complete the 'dynamicArray' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. 2D_INTEGER_ARRAY queries
+     */
+
+    public static List<int> dynamicArray(int n, List<List<int>> queries)
+    {
+        var result = queries[0];
+
+        return result;
+
+
+    }
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+
+            dynamicArray(2, [[1, 2], [2, 3]]);
+    }
+}
+
+}
