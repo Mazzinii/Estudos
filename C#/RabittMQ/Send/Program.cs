cc03b3717680225be6c0616693662a063afd85a0
Mazzinii <luizeduardomazzini@gmail.com>
2025-07-22 20:55:31 -0300

    feat: Learn more about RabbitMQ

--- C#/RabittMQ/Send/Program.cs
+++ C#/RabittMQ/Send/Program.cs
@@ -7,11 +7,28 @@ using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
 await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
+await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
 
-const string message = "Hello World!";
-var body = Encoding.UTF8.GetBytes(message);
 
-await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+Console.WriteLine("Send your message (Send exit to exit):");
+Console.WriteLine();
+
+while (true)
+{
+    string message = Console.ReadLine();
+
+    if (message == "exit") break;
+
+    var body = Encoding.UTF8.GetBytes(message!);
+
+    var properties = new BasicProperties
+    {
+        Persistent = true,
+    };
+
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "hello", body: body);
+}
+
 
 Console.WriteLine(" Press [enter] to exit.");
 Console.WriteLine();
--- C#/RabittMQ/Worker/Program.cs
+++ C#/RabittMQ/Worker/Program.cs
@@ -7,8 +7,6 @@ using var connection = await factory.CreateConnectionAsync();
 using var channel = await connection.CreateChannelAsync();
 
 await channel.QueueDeclareAsync(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-await channel.QueueDeclareAsync(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
-await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
 
 Console.WriteLine(" [*] Waiting for messages:");
 
@@ -18,6 +16,11 @@ consumer.ReceivedAsync += async (model, ea) =>
     var body = ea.Body.ToArray();
     var message = Encoding.UTF8.GetString(body);
     Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Split('.').Length - 1;
+    await Task.Delay(dots * 1000);
+    Console.WriteLine("[x] Done");
+
     await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
 };
 
